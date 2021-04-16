2a735f3f2cf514783d118700691c6658e2ab0ef8
Shirley <shirley_dong@brown.edu>
2021-04-16 14:26:15 -0700

    hermann grids rgb values and hermannGrid function to create hermann grids

---
+++ hermannGrid.m
@@ -0,0 +1,45 @@
+% % hermannGrid.m
+% written on April 3, 2021
+% by Shirley Dong
+% This script contains the function hermannGrid that creates colored Hermann grids.
+
+
+function [grid] = hermannGrid(r_border,g_border,b_border,r_square,g_square,b_square)
+% creates hermann grids 
+% create row for grid
+row = [zeros(1,1,3) ones(1,10,3) zeros(1,1,3)];
+% imshow(row);
+
+% create column for grid
+col = imrotate(row,270);
+% imshow(col);
+
+    % create black and white grid
+bw_grid = repmat([col.*row],6,6);
+% imshow(bw_grid);
+
+[M,N,D] = size(bw_grid);
+% get dimensions of grid
+
+grid = bw_grid;
+
+for ii = 1:M
+% loop through rows of grid
+    for jj = 1:N
+    % loop through columns of grid
+        rgb = reshape(grid(ii,jj,:),3,1);
+        % get RGB values at grid location ii,jj
+        if rgb == zeros(3,1)
+        % check if RGB values at ii,jj are 0s
+            grid(ii,jj,:) = cat(3,r_border,g_border,b_border);
+            % change border color
+        end
+        if rgb == ones(3,1)
+        % check if RGB values at ii,jj are 1s
+            grid(ii,jj,:) = cat(3,r_square,g_square,b_square);
+            % change square color
+        end
+    end
+end
+end
+
---
+++ hermann_grid.m
@@ -0,0 +1,29 @@
+% hermann_grid.m
+% written on April 3, 2021
+% by Shirley Dong
+% This script contains the RGB values for the Hermann grids.
+
+
+% regular red and green - the colors that actually displayed
+% red = rgb(233,63,51)
+% green = rgb(127,242,26)
+
+% 20 percent darker red and green
+% dark red = 0.7294, 0.1961, 0.1608
+% dark green = 0.4000, 0.7608, 0
+
+% 20 percent lighter red and green
+% light red = 0.9294, 0.3961, 0.3608
+% light green = 0.6000, 0.9608, 0.2000
+
+% regular blue and yellow - the colors that actually displayed
+% blue = rgb(36,74,252)
+% yellow = rgb(253,247,49)
+
+% 20 percent darker blue and yellow
+% dark blue = 0, 0.2275, 0.7882
+% dark yellow = 0.8000, 0.7765, 0.1608
+
+% 20 percent lighter blue and yellow
+% light blue = 0.2000, 0.4275, 0.9882
+% light yellow = 1, 0.9765, 0.3608
